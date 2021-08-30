269ca799619ae8020ac4e64f7124cc13a4e73a10
Łukasz Kołodziejski <lukasz-kolodziejski3333@wp.pl>
2021-08-30 23:26:28 +0200

    App. js

--- App.js
+++ App.js
@@ -1,24 +1,28 @@
 import React from "react";
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
+import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import productsReducer from "./store/reducers/products";
 import cartReducer from "./store/reducers/cart";
 import orderReducer from "./store/reducers/order";
-import ShopNavigator from "./navigation/ShopNavigator";
+import authReducer from "./store/reducers/auth";
+import NavigationContainer from "./navigation/NavigationContainer";
 
 const rootReducer = combineReducers({
   products: productsReducer,
   cart: cartReducer,
   order: orderReducer,
+  auth: authReducer,
 });
 
-const store = createStore(rootReducer, composeWithDevTools());
+// const store = createStore(rootReducer, composeWithDevTools());
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 const App = () => {
   return (
     <Provider store={store}>
-      <ShopNavigator />
+      <NavigationContainer />
     </Provider>
   );
 };
