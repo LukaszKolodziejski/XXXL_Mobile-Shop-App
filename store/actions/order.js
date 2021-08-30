5613b99783c30a152a088ae6c422b6c36eb91e91
Łukasz Kołodziejski <lukasz-kolodziejski3333@wp.pl>
2021-08-30 23:30:43 +0200

    add redux actions

--- store/actions/order.js
+++ store/actions/order.js
@@ -1,9 +1,51 @@
-export const ADD_ORDER = "ADD_ORDER";
+import axios from "../../data/axios-products";
+import Order from "../../models/Order";
 
-export const addOrder = (items, amount) => ({
-  type: ADD_ORDER,
-  orderData: {
-    items,
-    amount,
-  },
-});
+export const ADD_ORDER = "ADD_ORDER";
+export const SET_ORDER = "SET_ORDER";
+
+export const fetchOrders = () => {
+  return async (dispatch, getState) => {
+    const userId = getState().auth.userId;
+    await axios.get(`/orders/${userId}.json`).then((res) => {
+      const orders = [];
+      for (const key in res.data) {
+        const newData = new Order(
+          key,
+          res.data[key].items,
+          res.data[key].amount,
+          res.data[key].date
+        );
+        orders.push(newData);
+      }
+      dispatch({
+        type: SET_ORDER,
+        orders,
+      });
+    });
+  };
+};
+
+export const addOrder = (items, amount) => {
+  return async (dispatch, getState) => {
+    const { token, userId } = getState().auth;
+    const date = new Date();
+    await axios
+      .post(`/orders/${userId}.json?auth=${token}`, {
+        items,
+        amount,
+        date: date.toISOString(),
+      })
+      .then((res) => {
+        dispatch({
+          type: ADD_ORDER,
+          orderData: {
+            id: res.data.name,
+            items,
+            amount,
+            date,
+          },
+        });
+      });
+  };
+};
